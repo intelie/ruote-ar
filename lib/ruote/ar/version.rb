9898a1c5c855bb8c512f65a7981c65fdb7323fea
jiang chaofan <jiangchaofan@gmail.com>
2012-10-18 00:26:11 +0800

    jiang init commit 0.0.2 after forking

---
+++ lib/ruote/ar/version.rb
@@ -0,0 +1,11 @@
+#--
+# forked from https://github.com/sergiojunior/ruote-ar at 2012-10-10 by jiangchaofan@gmail.com
+#++
+
+
+module Ruote
+module ActiveRecord
+
+  VERSION = '0.0.2'
+end
+end
--- test/functional_connection.rb
+++ test/functional_connection.rb
@@ -1,22 +1,64 @@
+#
+# testing ruote-ar
+#
+# Thu Feb 10 11:14:56 JST 2011
+#
+
+require 'rufus-json/automatic'
 require 'ruote-ar'
 
-class RrLogger
-  def method_missing (m, *args)
-    super if args.length != 1
-    puts ". #{Time.now.to_f} #{Thread.current.object_id} #{args.first}"
-  end
-end
+# ENV['RUOTE_STORAGE_DB'] = "mysql2"
+# unless $sequel
+# 
+#   $sequel = case ENV['RUOTE_STORAGE_DB'] || 'postgres'
+#     when 'pg', 'postgres'
+#       Sequel.connect('postgres://localhost/ruote_test')
+#     when 'my', 'mysql'
+#       #Sequel.connect('mysql://root:root@localhost/ruote_test')
+#       Sequel.connect('mysql://root@localhost/ruote_test')
+#     when 'mysql2'
+#       Sequel.connect('mysql2://erp_user_d:adiFa81f83nDk@localhost/erpdb_d')
+#       # Sequel.connect('mysql2://root@localhost/ruote_test')
+#     when /:/
+#       Sequel.connect(ENV['RUOTE_STORAGE_DB'])
+#     else
+#       raise ArgumentError.new("unknown DB: #{ENV['RUOTE_STORAGE_DB'].inspect}")
+#   end
+# 
+#   require 'logger'
+# 
+#   logger = case ENV['RUOTE_STORAGE_DEBUG']
+#     when 'log'
+#       FileUtils.rm('debug.log') rescue nil
+#       Logger.new('debug.log')
+#     when 'stdout'
+#       Logger.new($stdout)
+#     else
+#       nil
+#   end
+# 
+#   if logger
+#     logger.level = Logger::DEBUG
+#     $sequel.loggers << logger
+#   end
+# 
+#   Ruote::Sequel.create_table($sequel, true)
+#     # true forces re_create of 'documents' table
+# end
 
 
-
-def new_storage (opts)
+# eg.
+# opts = {
+#  :adapter => 'mysql2',
+#  :database => 'db',
+#  :username => 'user', 
+#  :password => 'passwd',                     
+#  :pool => 10, #needed for eft_18
+#  :host => 'localhost'
+# }
+def new_storage(opts)
   # Database credentials
-  ::ActiveRecord::Base.establish_connection({:adapter => 'mysql2',
-                                              :database => 'itsm_test',
-                                              :username => 'root',                      
-                                              :pool => 10, #needed for eft_18
-                                              :host => 'localhost'})
+  ::ActiveRecord::Base.establish_connection(opts)
   
   ::Ruote::ActiveRecord::Storage.new(opts)  
 end
-
